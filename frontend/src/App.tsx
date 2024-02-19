7210528e3824ad9d7a9bdc5d8ca234f81554739e
mryan-3 <malawaryan@gmail.com>
2024-02-19 23:33:11 +0300

    Added the doctors section

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,12 @@
 import Navbar from '../src/components/Navbar.tsx'
+import Doctor from './components/Doctor.tsx'
 import Hero from './components/Hero.tsx'
 function App() {
-
   return (
     <div className='bg-gradient'>
-    <Navbar />
-    <Hero />
-    <div />
+      <Navbar />
+      <Hero />
+      <Doctor />
     </div>
   )
 }
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,12 +1,22 @@
 import type { Config } from 'tailwindcss'
 
-export default {
+const config = {
+  darkMode: ['class'],
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
+  prefix: '',
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
     fontFamily: {
       merriweather: ['Merriweather', 'serif', 'bold'],
       montserrat: ['Montserrat', 'sans-serif'],
@@ -16,24 +26,59 @@ export default {
     },
     extend: {
       colors: {
-        // Primary
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
         primary: '#b2d7de',
-
-        // Neutral
         secondary: '#eae2f0',
-
-        // Accent
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
+        },
         accent: '#c2e0cd',
+        und: 'hsl(var(--accent-foreground))',
 
-        // Error
-        error: '#e3342f',
-
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))',
+        },
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
+        },
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
+      },
+      backgroundImage: {
+        gradient:
+          'linear-gradient(90deg, rgba(178,215,222,1) 0%, rgba(234,226,240,1) 71%, rgba(194,224,205,1) 100%)',
       },
     },
-    backgroundImage: {
-      gradient:
-        'linear-gradient(90deg, rgba(178,215,222,1) 0%, rgba(234,226,240,1) 71%, rgba(194,224,205,1) 100%)',
-    },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animate')],
 } satisfies Config
+
+export default config
