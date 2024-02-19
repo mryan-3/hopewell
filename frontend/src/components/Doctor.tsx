0bd8012206c740e66dd860660184a35180a8de57
mryan-3 <malawaryan@gmail.com>
2024-02-19 23:32:48 +0300

    Added the doctors section

---
+++ frontend/src/components/Doctor.tsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import DoctorImage from '../assets/doctor.jpeg'
+
+const Doctor: React.FC = () => {
+  return (
+    <div className='w-full h-full bg-gradient p-8'>
+      <hr />
+      <div className='flex justify-center items-center'>
+        <h1 className='font-montserrat font-bold text-[40px] py-4'>
+          {' '}
+          Our Doctor
+        </h1>
+      </div>
+      <br />
+      <div className='flex flex-col lg:flex-row '>
+        <div className='flex flex-col w-full p-4 '>
+          <img
+            src={DoctorImage}
+            className='my-auto  rounded-lg border-accent border-4'
+          />
+        </div>
+        <div className='flex flex-col px-2 '>
+          <h1 className='font-montserrat font-bold text-[20px] p-2 text-center'>
+            Dr Amara Kapoor
+          </h1>
+          <p className='font-open-sans text-[16px] font-medium p-2 text-center'>
+            Dr. Amara Kapoor is a board-certified gynecologist dedicated to
+            offering comprehensive and compassionate care for women throughout
+            all stages of life. With extensive experience and a commitment to
+            patient-centered care, Dr. Kapoor strives to build lasting
+            relationships with her patients and empower them to achieve optimal
+            health.
+          </p>
+          <h2 className='font-montserrat font-bold text-[20px] p-2 text-center'>
+            Education & Qualification
+          </h2>
+          <p className='font-open-sans text-[16px] font-medium p-2'>
+              <li className='py-1'>
+                Harvard Medical School, Boston, MA (2017)
+              </li>
+              <li className='py-1'>
+                Residency in Obstetrics & Gynecology, Johns Hopkins Hospital,
+                Baltimore, MD (2022)
+              </li>
+              <li className='py-1'>
+                Board-certified in Obstetrics & Gynecology by the American Board
+                of Obstetrics and Gynecology (ABOG)
+              </li>
+              <li className='py-1'>
+                Fellowship in Minimally Invasive Gynecological Surgery, Mayo
+                Clinic, Rochester, MN (2023)
+              </li>
+          </p>
+          <h2 className='font-montserrat font-bold text-[20px] p-2 text-center'>
+            Areas of Expertise
+          </h2>
+          <p className='font-open-sans text-[16px] font-medium p-2'>
+              <li className='py-1'>Minimally invasive gynecological surgery</li>
+              <li className='py-1'>Comprehensive women's health care</li>
+              <li className='py-1'>Prenatal care and delivery</li>
+              <li className='py-1'>Family planning and contraception</li>
+              <li className='py-1'>
+                Management of common gynecological conditions (e.g.,
+                endometriosis, PCOS, menstrual disorders)
+              </li>
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Doctor
--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -1,25 +1,35 @@
 import React from 'react'
+import { Button } from '../../@/components/ui/button'
 
 const Hero: React.FC = () => {
   return (
-    <div className='bg-gradient flex justify-center items-center h-screen mb-12 '>
-    <div className='p-2 '>
-          <h2 className='font-montserrat font-semibold text-[28px] text-center md:text-[36px] p-4'>
-            Your Journey to Well-being Starts Here: Compassionate Gynecology at
-            <hr />
-            <span className='text-[20px] italic p-4 font-bold'>
-              {' '}
-              Hopewell General Hospital
-            </span>
-            <hr />
-          </h2>
-          <p className='font-open-sans text-[16px] font-semibold text-center p-4'>
-            Offering Comprehensive Gynecological Care, From Routine Checkups to
-            Advanced Procedures.
-          </p>
+    <div className='bg-gradient flex justify-center items-center h-screen '>
+      <div className='p-2 '>
+        <h2 className='font-montserrat font-semibold text-[28px] text-center md:text-[36px] p-4'>
+          Your Journey to Well-being Starts Here: Compassionate Gynecology at
           <hr />
-          <button>Book</button>
+          <span className='text-[20px] italic p-4 font-bold'>
+            {' '}
+            Hopewell General Hospital
+          </span>
+          <hr />
+        </h2>
+        <p className='font-open-sans text-[16px] font-semibold text-center p-4'>
+          Offering Comprehensive Gynecological Care, From Routine Checkups to
+          Advanced Procedures.
+        </p>
+        <hr />
+        <div className='flex items-center justify-center w-full p-6'>
+        <button className='group relative inline-flex h-12 items-center justify-center overflow-hidden rounded-md border border-neutral-200 font-medium'>
+          <div className='inline-flex h-12 translate-x-0 items-center justify-center bg-accent px-6 text-neutral-950 transition group-hover:-translate-x-[150%]'>
+            Book An Appointment
+          </div>
+          <div className='absolute inline-flex h-12 w-full translate-x-[100%] items-center justify-center bg-accent px-6 text-black transition duration-300 group-hover:translate-x-0'>
+            Book An Appointment
+          </div>
+        </button>{' '}
         </div>
+      </div>
     </div>
   )
 }
