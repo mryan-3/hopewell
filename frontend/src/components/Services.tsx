9b08c678ff858d2b1f3d7ebe29f80f0aaebd3227
mryan-3 <malawaryan@gmail.com>
2024-02-28 21:12:52 +0300

    Updated the services section

--- frontend/src/components/Services.tsx
+++ frontend/src/components/Services.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Stethoscope } from 'lucide-react'
+import { Stethoscope, Microscope, Heart, ShieldPlus, ActivitySquare, PhoneCall} from 'lucide-react'
 
 const Services: React.FC = () => {
   return (
@@ -17,7 +17,7 @@ const Services: React.FC = () => {
         <div className='flex flex-col md:grid grid-rows-2 grid-cols-3 p-2'>
           <div className='border-accent border-4 p-2 mx-2 my-4 flex flex-col justify-evenly items-center'>
             <Stethoscope />
-            <h2 className='font-montserrat font-semibold text-12px p-4 '>
+            <h2 className='font-montserrat font-semibold text-center text-12px p-4 '>
               Preventive Care
             </h2>
             <p className='font-open-sans font-medium text-10px leading-6 text-center p-4'>
@@ -27,58 +27,61 @@ const Services: React.FC = () => {
             </p>
           </div>
           <div className='border-accent border-4 p-2 my-4 mx-2 flex flex-col justify-evenly items-center'>
-            <Stethoscope />
-            <h2 className='font-montserrat font-semibold text-12px p-4'>
-              Preventive Care
+            <Heart />
+            <h2 className='font-montserrat font-semibold text-center text-12px p-4'>
+              Women's Health Management{' '}
             </h2>
             <p className='font-open-sans font-medium text-10px leading-6 text-center p-4'>
-              We offer comprehensive preventive care services, including annual
-              exams, Pap smears, STI screenings, breast cancer screenings, and
-              bone density scans.{' '}
+              We understand the unique challenges and concerns women face
+              throughout their lives. We offer a wide range of services to
+              support your overall health and well-being.
             </p>
           </div>
           <div className='border-accent border-4 p-2 my-4 mx-2 flex flex-col justify-evenly items-center'>
-            <Stethoscope />
-            <h2 className='font-montserrat font-semibold text-12px p-4 '>
-              Preventive Care
+            <ActivitySquare />
+            <h2 className='font-montserrat font-semibold text-center text-12px p-4 '>
+              Gynecological Conditions
             </h2>
             <p className='font-open-sans font-medium text-10px text-center leading-6 p-4'>
-              We offer comprehensive preventive care services, including annual
-              exams, Pap smears, STI screenings, breast cancer screenings, and
-              bone density scans.{' '}
+              We diagnose and treat a variety of common conditions, such as
+              endometriosis, polycystic ovary syndrome (PCOS), uterine fibroids,
+              pelvic pain, and vaginitis. We utilize advanced diagnostic tools
+              whenever possible to ensure optimal outcomes.
             </p>
           </div>
           <div className='row-start-2 border-accent border-4 p-2 my-4 mx-2 flex flex-col justify-evenly items-center'>
-            <Stethoscope />
-            <h2 className='font-montserrat font-semibold text-12px p-4 '>
-              Preventive Care
+            <Microscope />
+            <h2 className='font-montserrat font-semibold text-12px text-center p-4 '>
+              Surgical Services
             </h2>
             <p className='font-open-sans font-medium text-10px text-center leading-6 p-4'>
-              We offer comprehensive preventive care services, including annual
-              exams, Pap smears, STI screenings, breast cancer screenings, and
-              bone density scans.{' '}
+              We offer a range of surgical procedures, including laparoscopic
+              surgery, hysterectomy, and other gynecological procedures tailored
+              to your specific needs. We prioritize patient safety and comfort
+              throughout the entire surgical process.
             </p>
           </div>
           <div className='row-start-2 border-accent border-4 p-2 my-4 mx-2 flex flex-col justify-evenly items-center'>
-            <Stethoscope />
-            <h2 className='font-montserrat font-semibold text-12px p-4 '>
-              Preventive Care
+            <ShieldPlus/>
+            <h2 className='font-montserrat font-semibold text-center text-12px p-4 '>
+              Wellness and Intimacy
             </h2>
             <p className='font-open-sans font-medium text-10px text-center leading-6 p-4'>
-              We offer comprehensive preventive care services, including annual
-              exams, Pap smears, STI screenings, breast cancer screenings, and
-              bone density scans.{' '}
+              e recognize that a woman's well-being extends beyond her physical
+              health. We offer resources and support to help you navigate the
+              various aspects of well-being.
             </p>
           </div>
           <div className='row-start-2 border-accent border-4 p-2 mx-2 my-4 flex flex-col justify-evenly items-center'>
-            <Stethoscope />
-            <h2 className='font-montserrat font-semibold text-12px p-4 '>
-              Preventive Care
+            <PhoneCall />
+            <h2 className='font-montserrat font-semibold text-12px text-center p-4 '>
+              Telehealth Services
             </h2>
             <p className='font-open-sans font-medium text-10px text-center leading-6 p-4'>
-              We offer comprehensive preventive care services, including annual
-              exams, Pap smears, STI screenings, breast cancer screenings, and
-              bone density scans.{' '}
+              We understand that access to healthcare can be a challenge for
+              some patients. We offer convenient telehealth consultations for
+              Initial consultations, follow-up appointments and post-operative
+              support.
             </p>
           </div>
         </div>
